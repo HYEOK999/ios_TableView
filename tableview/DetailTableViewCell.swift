bb9da14b40abded782e282fe384ceff98f759f81
KimJunHyeok <iasg2004>
2019-06-28 01:40:00 +0900

    테이블 디자인 추가 및 각 VC, tableviewcell 파일 생성

---
+++ tableview/DetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailTableViewCell.swift
+//  tableview
+//
+//  Created by HYEOKBOOK on 28/06/2019.
+//  Copyright © 2019 HYEOKBOOK. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
