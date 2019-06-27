33c5a2627fde0ade853e5fb143e020607f36d43c
KimJunHyeok <iasg2004>
2019-06-28 01:36:13 +0900

    first commit

---
+++ tableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableview
+//
+//  Created by HYEOKBOOK on 28/06/2019.
+//  Copyright © 2019 HYEOKBOOK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
